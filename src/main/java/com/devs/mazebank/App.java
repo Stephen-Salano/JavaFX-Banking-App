11a3cacd64af3d07d5a23dc4b8e12e8724fb4b56
Salano <xirlanostephen@gmail.com>
2025-02-17 15:42:21 +0300

    Creating Login Page UI

--- src/main/java/com/devs/mazebank/App.java
+++ src/main/java/com/devs/mazebank/App.java
@@ -1,7 +1,20 @@
 package com.devs.mazebank;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * This is the entry point of the Application
  */
-public class App {
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/Login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/devs/mazebank/Controllers/LoginController.java
+++ src/main/java/com/devs/mazebank/Controllers/LoginController.java
@@ -1,4 +1,13 @@
 package com.devs.mazebank.Controllers;
 
+import javafx.scene.control.*;
+
 public class LoginController {
+    public ChoiceBox account_selector;
+    public Label payee_address_lbl;
+    public TextField payee_adress_field;
+    public Label password_lbl;
+    public PasswordField password_field;
+    public Button login_btn;
+    public Label error_lbl;
 }
