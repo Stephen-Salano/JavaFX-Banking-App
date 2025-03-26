49a085652cf70c910b50d2a8ad7182c5f8f86978
Salano <xirlanostephen@gmail.com>
2025-03-26 21:24:32 +0300

    feat(accounts): Implement database method to fetch client account details
    - Add getCheckingAccountInfo method in DatabaseDriver to retrieve
      checking account information by payee address
    - Retrieve account details including account number, transaction limit,
      balance, and owner
    - Implement account details loading in AccountsController

--- src/main/java/com/devs/mazebank/Controllers/Client/AccountsController.java
+++ src/main/java/com/devs/mazebank/Controllers/Client/AccountsController.java
@@ -1,5 +1,9 @@
 package com.devs.mazebank.Controllers.Client;
 
+import com.devs.mazebank.Models.CheckingAccount;
+import com.devs.mazebank.Models.Model;
+import com.devs.mazebank.Models.SavingsAccount;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -22,8 +26,45 @@ public class AccountsController implements Initializable {
     public TextField ammount_to_checkings;
     public Button transfer_to_checkings_btn;
 
+    private Model model;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.model = Model.getInstance();
 
+        loadCheckingAccountDetails();
+        loadSavingsAccountDetails();
+    }
+
+    public void loadCheckingAccountDetails(){
+        // get client Payee address
+        String clientPayeeAddress = model.getClient().payeeAddressProperty().get();
+
+        // get the client checking account details
+        ObservableList<CheckingAccount> clientAccountDetails = model.getDatabaseDriver().getCheckingAccountInfo(clientPayeeAddress);
+        // Setting checking Account Details details from the database
+        for(CheckingAccount checkingAccount: clientAccountDetails){
+            check_acc_num.setText(checkingAccount.accountNumberProperty().get());
+            transaction_limit.setText(String.valueOf(checkingAccount.transactionLimitProperty().get()));
+            check_acc_date.setText(String.valueOf(model.getClient().dateCreatedProperty().get()));
+            check_acc_bal.setText(String.valueOf(checkingAccount.balanceProperty().get()));
+        }
+
+    }
+    public void loadSavingsAccountDetails(){
+
+        // get the client payee address
+        String clientPayeeAddress = model.getClient().payeeAddressProperty().get();
+
+        // get the client savingsAccount details
+        ObservableList<SavingsAccount> savingsAccountObservableList = model.getDatabaseDriver().getSavingsAccountInfo(clientPayeeAddress);
+
+        // Setting the Savings Account details from the database
+        for (SavingsAccount savingsAccount: savingsAccountObservableList){
+            saving_acc_num.setText(savingsAccount.accountNumberProperty().get());
+            savings_withdraw_lim.setText(String.valueOf(savingsAccount.withdrawalLimitProperty().get()));
+            savings_acc_date.setText(String.valueOf(model.getClient().dateCreatedProperty().get()));
+            savings_acc_bal.setText(String.valueOf(savingsAccount.balanceProperty().get()));
+        }
     }
 }
